a0c3deb33c49b735044eaaa73c35284fb31e0dc5
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2023-04-26 17:30:01 -0300

    routes

--- implementacao/moeda-estudantil/crud/src/pages/home.jsx
+++ implementacao/moeda-estudantil/crud/src/pages/home.jsx
@@ -1,10 +1,10 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return(
         <div style={{display: "flex", flexDirection: "column"}}>
             <h1>CRUDs</h1>
-            <Link to={"/aluno"} >empresa</Link>
+            <Link to={"/aluno"} >aluno</Link>
             <Link to={"/empresa"}>empresa</Link>
         </div>
     )
