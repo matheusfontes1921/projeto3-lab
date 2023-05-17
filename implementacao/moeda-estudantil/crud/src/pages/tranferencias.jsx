36ae958e77da43282201ecfc04bed6b5bff94df9
andresathler <sathmory@gmail.com>
2023-05-17 16:00:39 -0300

    tranferencia e transacao

--- implementacao/moeda-estudantil/crud/src/pages/tranferencias.jsx
+++ implementacao/moeda-estudantil/crud/src/pages/tranferencias.jsx
@@ -1,10 +1,11 @@
 import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 export default function Transferencias() {
     const { id } = useParams();
     const [transferencias, setTransferencias] = useState([])
+    const [saldo, setSaldo] = useState(0)
 
     useEffect(() => {
         const tipo = window.location.pathname.split('/')[1]
@@ -16,19 +17,46 @@ export default function Transferencias() {
                         setTransferencias(response.data)
                     })
                     .catch(response => {console.log(response)})
+                axios.get(`http://localhost:8080/professores/saldo/${id}`, {headers: { "Content-Type": "application/json" }})
+                    .then(res => {
+                        console.log(res)
+                        setSaldo(res.data)
+                    })
+                    .catch(res => console.log(res))
             } else if(tipo === 'aluno') {
+                document.getElementById("back").style.display = "none";
+
                 axios.get(`http://localhost:8080/alunos/transferencias/${id}`)
                     .then((response) => {
                         setTransferencias(response.data)
                     })
+
+                axios.get(`http://localhost:8080/alunos/saldo/${id}`, {headers: { "Content-Type": "application/json" }})
+                    .then(res => {
+                        console.log(res)
+                        setSaldo(res.data)
+                    })
+                    .catch(res => console.log(res))
             }
 
         }
     }, [transferencias]);
 
+    function back() {
+        window.location.pathname = document.location.pathname.split("transferencias")[0]
+    }
+
     return(
         <div style={{display: "flex", flexDirection: "column", width: "98vw", height: "90vh", alignItems: "center", justifyContent: "flex-start"}}>
+            <div id={"back"} onClick={back} style={{cursor: "pointer"}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     className="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <path fillRule="evenodd"
+                          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+            </div>
             <h1>Transferencias</h1>
+            <h3>Saldo: {saldo}</h3>
             <ul style={{listStyle: 'none'}}>
                 {transferencias.map(transferencia => {
                     console.log("t", transferencia)
@@ -45,6 +73,7 @@ export default function Transferencias() {
                         >
                             <span>id da transferencia: {transferencia.id}</span>
                             <span>valor: {transferencia.valor}</span>
+                            <span>descricao: {transferencia.descricao}</span>
                         </li>
                     )
                 })}
--- implementacao/moeda-estudantil/crud/src/pages/transacao.jsx
+++ implementacao/moeda-estudantil/crud/src/pages/transacao.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../assets/transacao.css';
 import {useParams} from "react-router-dom";
 import axios from "axios";
@@ -8,26 +8,80 @@ function Transacao() {
     const [aluno, setAluno] = useState(0);
     const [quantidade, setQuantidade] = useState(0);
     const [descricao, setDescricao] = useState(0);
+    const [saldo, setSaldo] = useState(0);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/professores/saldo/${id}`, {headers: { "Content-Type": "application/json" }})
+            .then(res => {
+                console.log(res)
+                setSaldo(res.data)
+            })
+            .catch(res => console.log(res))
+    }, []);
+
+
 
 
     function handleTransfer() {
         event.preventDefault()
 
-        axios.post(`http://localhost:8080/transferencia/${id}/${aluno}/${quantidade}`, {headers: { "Content-Type": "application/json" }})
-            .then(res => console.log(res.status))
-            .catch(res => console.log(res))
+        if(quantidade < 0) {
+            const alert = document.getElementById("alert");
+            alert.innerHTML = "Valor invalido";
+            alert.style.color = "red";
+            alert.style.display = "inline-block"
+        } else if(saldo - quantidade < 0) {
+            const alert = document.getElementById("alert");
+            alert.innerHTML = "Saldo insuficiente";
+            alert.style.color = "red";
+            alert.style.display = "inline-block"
+        } else {
+            axios.post(`http://localhost:8080/transferencia/${id}/${aluno}/${quantidade}/${descricao}`, {headers: { "Content-Type": "application/json" }})
+                .then(res => {
+                    if (res.status === 201) {
+                        setSaldo(saldo - quantidade);
+                        const alert = document.getElementById("alert");
+                        alert.innerHTML = "Created " + res.status;
+                        alert.style.color = "green";
+                        alert.style.display = "inline-block"
+                    } else {
+                        const alert = document.getElementById("alert");
+                        alert.innerHTML = "Error" + res.status;
+                        alert.style.color = "red";
+                        alert.style.display = "inline-block"
+                    }
+
+                })
+                .catch(res => console.log(res))
+        }
+
+
+    }
+
+    function clearAlert() {
+        const alert = document.getElementById("alert");
+        alert.style.display = "none"
     }
 
     function back() {
-        window.location.pathname = document.location.pathname.split("crud")[0]
+        window.location.pathname = document.location.pathname.split("transacao")[0]
     }
 
     return (
         <div className="super-container">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FAEDB7" fillOpacity="1" d="M0,160L12.6,165.3C25.3,171,51,181,76,192C101.1,203,126,213,152,218.7C176.8,224,202,224,227,208C252.6,192,278,160,303,154.7C328.4,149,354,171,379,176C404.2,181,429,171,455,154.7C480,139,505,117,531,138.7C555.8,160,581,224,606,229.3C631.6,235,657,181,682,149.3C707.4,117,733,107,758,96C783.2,85,808,75,834,74.7C858.9,75,884,85,909,96C934.7,107,960,117,985,138.7C1010.5,160,1036,192,1061,218.7C1086.3,245,1112,267,1137,277.3C1162.1,288,1187,288,1213,261.3C1237.9,235,1263,181,1288,165.3C1313.7,149,1339,171,1364,176C1389.5,181,1415,171,1427,165.3L1440,160L1440,0L1427.4,0C1414.7,0,1389,0,1364,0C1338.9,0,1314,0,1288,0C1263.2,0,1238,0,1213,0C1187.4,0,1162,0,1137,0C1111.6,0,1086,0,1061,0C1035.8,0,1011,0,985,0C960,0,935,0,909,0C884.2,0,859,0,834,0C808.4,0,783,0,758,0C732.6,0,707,0,682,0C656.8,0,632,0,606,0C581.1,0,556,0,531,0C505.3,0,480,0,455,0C429.5,0,404,0,379,0C353.7,0,328,0,303,0C277.9,0,253,0,227,0C202.1,0,177,0,152,0C126.3,0,101,0,76,0C50.5,0,25,0,13,0L0,0Z"></path></svg>
+            <div onClick={back} style={{cursor: "pointer"}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     className="bi bi-arrow-left" viewBox="0 0 16 16">
+                    <path fillRule="evenodd"
+                          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+            </div>
             <div className="container">
+                <div id={"alert"} onClick={clearAlert} style={{display: "none", cursor: "pointer"}}></div>
                 <h1>Transação de Moedas</h1>
                   <div className="transaction-form">
+                    <h3>Saldo: {saldo}</h3>
                     <div className="form-row">
                         <label htmlFor="aluno">Aluno:</label>
                         <input
