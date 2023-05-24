a6559d9b9e353bd113ba302ac8c5e01235f2e966
andresathler <sathmory@gmail.com>
2023-05-24 14:30:42 -0300

    email funcionando

--- implementacao/moeda-estudantil/backend/src/main/java/com/example/backend/controllers/EmailController.java
+++ implementacao/moeda-estudantil/backend/src/main/java/com/example/backend/controllers/EmailController.java
@@ -12,7 +12,6 @@ public class EmailController {
 
     private final EmailService emailService;
 
-    @Autowired
     public EmailController(EmailService emailService) {
         this.emailService = emailService;
     }
